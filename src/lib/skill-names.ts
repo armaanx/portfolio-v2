25a97f0f458b8bff5c2e4f0fc5b093b04f7abe70
Armaan Mishra <armaanmishra48@gmail.com>
2024-07-19 07:30:45 +0530

    add skill list

---
+++ src/lib/skill-names.ts
@@ -0,0 +1,81 @@
+type Skill = {
+  name: string;
+};
+
+export const skillIcons: Skill[] = [
+  {
+    name: "HTML",
+  },
+  {
+    name: "CSS",
+  },
+  {
+    name: "TailwindCSS",
+  },
+  {
+    name: "JavaScript",
+  },
+  {
+    name: "TypeScript",
+  },
+  {
+    name: "React",
+  },
+  {
+    name: "Next.js",
+  },
+  {
+    name: "Framer Motion",
+  },
+  {
+    name: "Node.js",
+  },
+  {
+    name: "Express.js",
+  },
+  {
+    name: "trpc",
+  },
+  {
+    name: "Prisma",
+  },
+  {
+    name: "MongoDB",
+  },
+  {
+    name: "PostgreSQL",
+  },
+  {
+    name: "Firebase",
+  },
+  {
+    name: "Supabase",
+  },
+  {
+    name: "Java",
+  },
+  {
+    name: "Python",
+  },
+  {
+    name: "C++",
+  },
+  {
+    name: "Git",
+  },
+  {
+    name: "GitHub",
+  },
+  {
+    name: "Heroku",
+  },
+  {
+    name: "Docker",
+  },
+  {
+    name: "Postman",
+  },
+  {
+    name: "AWS",
+  },
+];
