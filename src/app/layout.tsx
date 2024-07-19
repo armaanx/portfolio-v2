e4f62519b770e2f2159e4d1912095d42cda28e32
Armaan Mishra <armaanmishra48@gmail.com>
2024-07-19 07:41:40 +0530

    update page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const sora = Sora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Armaan's Portfolio",
+  title: "Armaan Mishra",
 };
 
 export default function RootLayout({
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("dark bg-[#111010] antialiased", sora.className)}>
+      <body
+        className={cn(
+          "dark bg-[#111010] antialiased max-w-2xl mx-auto",
+          sora.className
+        )}
+      >
         {children}
         <footer className="border-t bottom-0 p-4 max-w-2xl mx-auto">
           <p className="text-xs text-muted-foreground text-center">
